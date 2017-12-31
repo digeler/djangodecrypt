337c853bc301e48e33899c39cd0b7a2e6dc08df7
digeler <dinorg@gmail.com>
2017-12-31 20:52:14 +0200

    working

--- web/views.py
+++ web/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse	
+from django.http import HttpResponse
+from django.conf import settings
+	
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello')
+	#return HttpResponse('Hello')
+	return render(request, 'web/index.html')
